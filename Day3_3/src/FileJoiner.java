640f4021fc9ca4ec25216254dbc2753b80b907c4
TranHanHieu <hanhieu.a3.namly.2012@gmail.com>
2016-08-26 19:25:05 +0700

    first commit

--- Day3_3/src/FileJoiner.java
+++ Day3_3/src/FileJoiner.java
@@ -1,13 +1,30 @@
-import java.io.BufferedOutputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.*;
 
 /**
  * Created by Hieu It on 8/25/2016.
  */
 public class FileJoiner {
-    public static void fileJoiner(String path) throws FileNotFoundException {
-        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(path));
+    public static void fileJoiner(String path) throws IOException {
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream("FileJoiner.txt"));
+        String[] splits = path.split(",");
+        String nameFile = "";
+        int a;
+        for (int i = 0; i < splits.length; i++) {
+            nameFile = splits[i];
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(nameFile));
+            while ((a = bufferedInputStream.read()) != -1) {
+                bufferedOutputStream.write(a);
+            }
+            bufferedInputStream.close();
+        }
+        bufferedOutputStream.close();
+    }
 
+    public static void main(String[] args) {
+        try {
+            fileJoiner("E:\\install.log.1.txt,E:\\install.log.2.txt,E:\\install.log.3.txt,E:\\install.log.4.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
